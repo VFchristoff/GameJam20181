2ac0d055498d05523f134d074505f675bebc948e
Vanessa <vfchristoff9@gmail.com>
2018-05-08 16:38:39 -0300

    Update: new script UI dragger

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour {
+
+	// Use this for initialization
+	public void quitGame () {
+		Debug.Log ("has quit");
+		Application.Quit ();
+	}
+}
